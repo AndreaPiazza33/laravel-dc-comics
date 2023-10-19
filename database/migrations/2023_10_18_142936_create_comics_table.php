d0818fd0644ceba77dbdb306982355143336d6e3
AndreaPiazza33 <andreapiazza190201@gmail.com>
2023-10-19 17:53:36 +0200

    feat: add seeder

--- database/migrations/2023_10_18_142936_create_comics_table.php
+++ database/migrations/2023_10_18_142936_create_comics_table.php
@@ -15,13 +15,13 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
-            $table->string('title',50);
+            $table->string('title');
             $table->text('description');
-            $table->string('thumb');
-            $table->string('price',10);
-            $table->string('series',25);
-            $table->string('sale_date',10);
-            $table->string('type',15);
+            $table->text('thumb');
+            $table->string('price');
+            $table->string('series');
+            $table->date('sale_date');
+            $table->string('type');
             $table->timestamps();
         });
     }
